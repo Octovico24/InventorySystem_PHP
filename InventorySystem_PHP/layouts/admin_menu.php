2a1796bf84c4c18254c57a7ce048456d9a75b4f4
Christian Octavico Kuncoro <72993502+Octovico24@users.noreply.github.com>
2024-01-29 14:49:40 +0700

    Add files via upload

---
+++ InventorySystem_PHP/layouts/admin_menu.php
@@ -0,0 +1,61 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>Pengelolaan Pengguna</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Kelola Grup</a> </li>
+      <li><a href="users.php">Kelola Pengguna</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Kategori</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Produk</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Kelola Produk</a> </li>
+       <li><a href="add_product.php">Tambahkan Produk</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Kelola Gambar</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-credit-card"></i>
+       <span>Penjualan</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="sales.php">Kelola Penjualan</a> </li>
+         <li><a href="add_sale.php">Tambahkan Penjualan</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-duplicate"></i>
+       <span>Rekap Penjualan</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="sales_report.php">Penjualan berdasarkan tanggal </a></li>
+        <li><a href="monthly_sales.php">Penjulalan Bulanan</a></li>
+        <li><a href="daily_sales.php">Penjualan harian</a> </li>
+      </ul>
+  </li>
+</ul>
